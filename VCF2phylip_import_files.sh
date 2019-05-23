23aa642c4d2a6ecbe93abb7802e3786c0c2e0e46
ZhaoTingzju <49446619+ZhaoTingzju@users.noreply.github.com>
2019-05-23 16:36:28 +0800

    Update VCF2phylip_import_files.sh

--- VCF2phylip_import_files.sh
+++ VCF2phylip_import_files.sh
@@ -5,6 +5,6 @@ vcftools --vcf $1 --bed $2 --out SNP_in_Gene --recode --keep-INFO-all
 plink --vcf SNP_in_Gene.recode.vcf --allow-extra-chr --distance-matrix --out Dis_matrix
 perl -pi -e "s/\t.*/        /" Dis_matrix.mdist.id
 wc -l Dis_matrix.mdist.id|perl -pi -e 's/ .*//' > tree_import.txt
-paste Dis_matrix.mdist.id Dis_matrix.mdist >> tree_import.txt
+cat Dis_matrix.mdist.id Dis_matrix.mdist >> tree_import.txt
 rm Dis_matrix*
 rm SNP_in_Gene.recode.vcf
