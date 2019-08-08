7b8f70bfe3ddf5354016f73b2a737c897f79785c
ZhaoTingzju <49446619+ZhaoTingzju@users.noreply.github.com>
2019-08-08 09:57:32 +0800

    Update VCF2phylip_import_files.sh

--- VCF2phylip_import_files.sh
+++ VCF2phylip_import_files.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#produce import file which can be used for phylogenetic tree construction for software phylip.
-#bash ./sample.vcf ./gene.bed
-vcftools --vcf $1 --bed $2 --out SNP_in_Gene --recode --keep-INFO-all
-plink --vcf SNP_in_Gene.recode.vcf --allow-extra-chr --distance-matrix --out Dis_matrix
+#produce import file  used for phylogenetic tree construction for software phylip.
+#bash ./sample.vcf 
+vcftools --vcf $1 --plink -out Tree
+plink --file Tree --allow-extra-chr --distance-matrix --out Dis_matrix
 perl -pi -e "s/\t.*/        /" Dis_matrix.mdist.id
 wc -l Dis_matrix.mdist.id|perl -pi -e 's/ .*//' > tree_import.txt
-cat Dis_matrix.mdist.id Dis_matrix.mdist >> tree_import.txt
+paste Dis_matrix.mdist.id Dis_matrix.mdist >> tree_import.txt
 rm Dis_matrix*
-rm SNP_in_Gene.recode.vcf
