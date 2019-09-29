c3ec15f13dc2cae5addd3d035a1545e8a6b24764
Tin_zt <49446619+ZhaoTingzju@users.noreply.github.com>
2019-09-29 18:45:27 +0800

    Update SNP_QC.sh

--- SNP_QC.sh
+++ SNP_QC.sh
@@ -85,3 +85,8 @@ wait
 -I D13_noindel.recode.vcf_biallelic.vcf \
 -O merge.vcf 
 
+vcftools --vcf merge.vcf --missing-indv
+awk '$5< 0.5{print $1}' out.imss > id.txt
+vcftools --vcf merge.vcf --keep id.txt --recode --recode-INFO-all --out merged_437
+
+nohup vcftools --vcf ./merged_437.recode.vcf --maf 0.05 --max-maf 0.95 --max-missing 0.9 --out merged_437_maf5maxmaf95maxmissing9 &
