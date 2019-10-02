c30b90b604445e491176a1166fbd1b8f2a4d70da
Tin_zt <49446619+ZhaoTingzju@users.noreply.github.com>
2019-10-02 13:30:47 +0800

    Update 08_IBD_analysis.sh

--- 08_IBD_analysis.sh
+++ 08_IBD_analysis.sh
@@ -39,3 +39,24 @@ $str=$a[2]."\t".$a[3]."\t"."$name_id\n";
 @b=split(" ",$str);
 print $b[0]."\t".$b[1]."\t$b[2]\t$b[4]\n";
 }
+
+### chrom length
+seqkit fx2tab -l TM-1_V2.1.fa |cut -f 1,4|head -n 26 > genome.txt
+perl -pi -e 's/A0//s' genome.txt
+perl -pi -e 's/A//s' genome.txt
+perl -pi -e 's/D01/14/s' genome.txt
+perl -pi -e 's/D02/15/s' genome.txt
+perl -pi -e 's/D03/16/s' genome.txt
+perl -pi -e 's/D04/17/s' genome.txt
+perl -pi -e 's/D05/18/s' genome.txt
+perl -pi -e 's/D06/19/s' genome.txt
+perl -pi -e 's/D07/20/s' genome.txt
+perl -pi -e 's/D08/21/s' genome.txt
+perl -pi -e 's/D09/22/s' genome.txt
+perl -pi -e 's/D10/23/s' genome.txt
+perl -pi -e 's/D11/24/s' genome.txt
+perl -pi -e 's/D12/25/s' genome.txt
+perl -pi -e 's/D13/26/s' genome.txt
+
+ bedtools makewindows -g genome.txt -s 10000 -w 10000 |awk '{print $0"\tbin"NR}' > 10kbin.bed
+
